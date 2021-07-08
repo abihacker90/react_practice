f5cfde9d36f20f11d8e4c86c76c92406874c021f
Adam Bihacker <adam.bihacker@gmail.com>
2021-07-08 09:42:44 +0200

    react practice

---
+++ first_practice/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Button from './components/button/button.js';
+
+function App() {
+  return (
+    <>
+    <div className="form">
+     <Button text="Send" />
+    </div>
+    <div className="form2">
+      <Button text="Send2" />
+    </div>
+    </>
+  );
+}
+
+export default App;
