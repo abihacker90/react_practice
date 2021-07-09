203dd4eb6045890dcbf24d93b193b651068a75bc
Adam Bihacker <adam.bihacker@gmail.com>
2021-07-09 10:46:51 +0200

    updated again

--- first_practice/src/components/button/button.js
+++ first_practice/src/components/button/button.js
@@ -2,29 +2,30 @@ import './button.css';
 import React, { useState} from 'react';
 
 
-let timer;
-let prevHTML;
 
 /* window.hello = "helló";
 console.log(hello); */
 
-function clickHappened(e) {
-    
-    
-    const goodHTML = "Goodbye";
-    
-    if (e.target.innerHTML !== goodHTML) {
-        prevHTML = e.target.innerHTML;
-        e.target.innerHTML = "Goodbye";   
-        timer = setTimeout(function(){e.target.innerHTML = prevHTML}, 3000)
-    } else {
-        clearTimeout(timer);
-        e.target.innerHTML = prevHTML;
-        console.log(prevHTML);
-    }
-}
 
 function Button(props) {
+
+    let timer;
+    let prevHTML;
+
+    function clickHappened(e) {
+        const goodHTML = "Goodbye";
+        
+        if (e.target.innerHTML !== goodHTML) {
+            prevHTML = e.target.innerHTML;
+            e.target.innerHTML = "Goodbye";   
+            timer = setTimeout(function(){e.target.innerHTML = prevHTML}, 3000)
+        } else {
+            clearTimeout(timer);
+            e.target.innerHTML = prevHTML;
+            console.log(prevHTML);
+        }
+    };
+
     return (
         <button onClick={clickHappened}>{props.text}</button>
     );
