6c136ec4fd4e847ec12f2fb643eba9fc00bc95a3
Adam Bihacker <adam.bihacker@gmail.com>
2021-07-09 10:44:28 +0200

    updated

--- first_practice/src/components/button/button.js
+++ first_practice/src/components/button/button.js
@@ -1,9 +1,33 @@
 import './button.css';
+import React, { useState} from 'react';
+
+
+let timer;
+let prevHTML;
+
+/* window.hello = "helló";
+console.log(hello); */
+
+function clickHappened(e) {
+    
+    
+    const goodHTML = "Goodbye";
+    
+    if (e.target.innerHTML !== goodHTML) {
+        prevHTML = e.target.innerHTML;
+        e.target.innerHTML = "Goodbye";   
+        timer = setTimeout(function(){e.target.innerHTML = prevHTML}, 3000)
+    } else {
+        clearTimeout(timer);
+        e.target.innerHTML = prevHTML;
+        console.log(prevHTML);
+    }
+}
 
 function Button(props) {
     return (
-        <button>{props.text}</button>
+        <button onClick={clickHappened}>{props.text}</button>
     );
-}
+} 
 
 export default Button;
